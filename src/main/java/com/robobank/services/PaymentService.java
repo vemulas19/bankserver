fe29e7083ef3cb336de267e9ba6969fa12bd93d1
sheshank <akula.sheshank999@gmail.com>
2019-06-25 21:27:45 +0530

    bankproject

---
+++ src/main/java/com/robobank/services/PaymentService.java
@@ -0,0 +1,23 @@
+package com.robobank.services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import com.robobank.pojo.PaymentInfo;
+
+@Path("/")
+public class PaymentService {
+
+	@POST
+	@Path("/processPayment")
+	@Consumes("application/json")
+	public Response processPayment(PaymentInfo paymentInfo) {
+		System.out.println(paymentInfo.getCardNumber());
+		System.out.println(paymentInfo.getHolderName());
+		//update payment details in database
+		return Response.status(200).entity("success").build();
+	}
+	
+}
